cdf0fc409bce140ee735543613fefdd8d8bfa13a
TanushreeJadhav03 <tanushreejadhav2703@gmail.com>
2024-10-22 21:56:09 +0530

    coderefactor

---
+++ src/main/java/com/example/fusion_ms/AWS.java
@@ -0,0 +1,10 @@
+package com.example.fusion_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AWS {
+    @GetMapping("/AWS")
+    public String getData() {return  "Welcome to Fusion-AWS class with EC2, S3, Lambda" ; }
+}
